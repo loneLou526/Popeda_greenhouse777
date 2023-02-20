bbf9539c7d285e7a0f87ab335d8884bb7c901ccb
loneLou526 <103879098+loneLou526@users.noreply.github.com>
2023-02-21 01:53:46 +0300

    Update alarm.ino

--- arduino/alarm.ino
+++ arduino/alarm.ino
@@ -1,9 +1,7 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-#define DHTPIN 13 // Тот самый номер пина, о котором упоминалось выше
-// Одна из следующих строк закоментирована. Снимите комментарий, если подключаете датчик DHT11 к arduino
-// DHT dht(DHTPIN, DHT22); //Инициация датчика
+#define DHTPIN 13 
 DHT dht(DHTPIN, DHT11);
 void setup() {
   Serial.begin(9600);
@@ -12,12 +10,12 @@ void setup() {
   dht.begin();
 }
 void loop() {
-  delay(2000); // 2 секунды задержки
+  delay(2000); 
   float h = dht.readHumidity(); //Измеряем влажность
   float t = dht.readTemperature(); //Измеряем температуру
   if (isnan(h) || isnan(t)) {  // Проверка. Если не удается считать показания, выводится «Ошибка считывания», и программа завершает работу
     Serial.println("Error");
-    digitalWrite(12, HIGH);
+    digitalWrite(12, HIGH); // В случае ошибки загорается диод на 12-м пине.
     digitalWrite(2, 0);
     return;
   }
